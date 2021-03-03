95e32f556c00b93a8908c5aee43d7ea2f08f9397
Karolina Frieszova <karolinafriesz@gmail.com>
2021-03-03 12:08:58 +0000

    data cleaning

--- scripts/data_cleaning.R
+++ scripts/data_cleaning.R
@@ -53,52 +53,56 @@ lending_loans <- lending_loans %>%
             acc_now_delinq, # 4 rows = 1 all have loan_status = fully paid
             delinq_amnt, # 2 rows with loan_status = fully paid
             application_type, # all applications are individual
-            fico_range_low, # highly correlated with fico_range_high
             mths_since_last_delinq, # 63.3 % missing values
             mths_since_last_record, # 91.4% missing vales
             out_prncp_inv, # highly correlated with out_prncp
-            last_fico_range_low # highly correlated with last_fico_range_high
+            last_fico_range_low, # highly correlated with last_fico_range_high
+            total_rec_prncp # highly correlated with total_pymnt (principal is the amount you borrowed without interest)
             )) %>% 
   drop_na(open_acc) # remove 32 rows as this rows are missing across multiple columns
 
 # feature engineering
 lending_loans <- lending_loans %>% 
-  mutate(default_loan = case_when(loan_status == "Fully Paid" ~ 0,
-                                  loan_status == "Current" ~ 0,
-                                  loan_status == "Does not meet the credit policy. Status:Fully Paid" ~ 0,
-                                  loan_status == "Charged Off" ~ 1,
-                                  loan_status == "In Grace Period" ~ 1,
-                                  loan_status == "Late (31-120 days)" ~ 1,
-                                  loan_status == "Late (16-30 days)" ~ 1,
-                                  loan_status == "Default" ~ 1,
-                                  loan_status == "Does not meet the credit policy. Status:Charged Off" ~ 1
+  mutate(default_loan = case_when(loan_status == "Fully Paid" ~ F,
+                                  loan_status == "Current" ~ F,
+                                  loan_status == "Does not meet the credit policy. Status:Fully Paid" ~ F,
+                                  loan_status == "Charged Off" ~ T,
+                                  loan_status == "In Grace Period" ~ T,
+                                  loan_status == "Late (31-120 days)" ~ T,
+                                  loan_status == "Late (16-30 days)" ~ T,
+                                  loan_status == "Default" ~ T,
+                                  loan_status == "Does not meet the credit policy. Status:Charged Off" ~ T
                                   )) %>% # abstraction: loan status normal = 0, default = 1
   select(-loan_status) %>% 
   rename("addr_state" = "state_name") %>% 
-  mutate(delinq_2yrs = ifelse(delinq_2yrs == 0, 0, 1), # past-due incidences of delinquency in the borrower's credit file for the past two years
-         inq_last_6mths  = ifelse(inq_last_6mths == 0, 0, 1),
-         pub_rec = ifelse(pub_rec == 0, 0, 1), # derogatory public records, no = 0, yes = 1
-         out_prncp = ifelse(out_prncp == 0, 0, 1), # Remaining outstanding principal for total amount funded, no = 0, yes = 1
-         total_rec_late_fee = ifelse(total_rec_late_fee == 0, 0, 1), # Late fees received to date, no = 0, yes = 1
-         recoveries = ifelse(recoveries == 0, 0, 1), # post charge off gross recovery, no = 0, yes = 1
-         collection_recovery_fee = ifelse(collection_recovery_fee == 0, 0, 1), # post charge off collection fee, no = 0, yes = 1
-         int_rate = str_remove_all(int_rate, "[%]"),
-         int_rate = as.numeric(int_rate), # convert interest rate to numeric
+  mutate(delinq_2yrs = ifelse(delinq_2yrs == 0, F, T), # past-due incidences of delinquency in the borrower's credit file for the past two years
+         inq_last_6mths  = ifelse(inq_last_6mths == 0, F, T),
+         pub_rec = ifelse(pub_rec == 0, F, T), # derogatory public records
+         out_prncp = ifelse(out_prncp == 0, F, T), # Remaining outstanding principal for total amount funded
+         total_rec_late_fee = ifelse(total_rec_late_fee == 0, F, T), # Late fees received to date
+         recoveries = ifelse(recoveries == 0, F, T), # post charge off gross recovery
+         collection_recovery_fee = ifelse(collection_recovery_fee == 0, F, T), # post charge off collection fee
+         int_rate_pct = str_remove_all(int_rate, "[%]"),
+         int_rate_pct = as.numeric(int_rate_pct), # convert interest rate to numeric
          issue_d = str_remove_all(issue_d, "[0-9-]"), # remove year, leave month
          issue_d = case_when(issue_d %in% c("Jan", "Feb", "Mar") ~ "Q1",
                              issue_d %in% c("Apr", "May", "Jun") ~ "Q2",
                              issue_d %in% c("Jul", "Aug", "Sep") ~ "Q3",
                              issue_d %in% c("Oct", "Nov", "Dec") ~ "Q4"),
          emp_length = recode(emp_length, "n/a" = "unknown"),
-         revol_util = str_remove_all(revol_util, "[%]"), # the amount of credit the borrower is using relative to all available revolving credit
-         revol_util = as.numeric(revol_util), # convert to numeric
+         addr_state = coalesce(addr_state, "unknown"),
+         revol_util_pct = str_remove_all(revol_util, "[%]"), # the amount of credit the borrower is using relative to all available revolving credit
+         revol_util_pct = as.numeric(revol_util_pct), # convert to numeric
          revol_util = coalesce(revol_util, median(revol_util, na.rm = TRUE)), # replace missing values with median
          pub_rec_bankruptcies = as.character(pub_rec_bankruptcies),
          pub_rec_bankruptcies = case_when(pub_rec_bankruptcies == "0" ~ "no", 
                                           pub_rec_bankruptcies == "1" ~ "yes",
                                           pub_rec_bankruptcies == "2" ~ "yes", 
                                           TRUE ~ "unknown"), # public record bankruptcies
-         install_mth_perc = round((installment * 100) / (annual_inc/12), 2), # monthly expense %
-         )
-
+         install_mth_pct = round((installment * 100) / (annual_inc/12), 2), # monthly installment expense %
+         fico_range_avg = (fico_range_low + fico_range_high) / 2
+         ) %>% 
+  mutate_if(is_character, as_factor) %>% 
+  select(-c(collection_recovery_fee, int_rate, revol_util, fico_range_low, fico_range_high))
+  
 write_csv(lending_loans, "clean_data/lending_loans.csv")
